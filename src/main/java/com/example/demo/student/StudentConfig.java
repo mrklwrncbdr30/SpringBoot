b137e145c6427f18a4e33166dc529cab09c21b17
LawR <marklawrenced.bedar@gmail.com>
2024-03-13 14:25:37 +0800

    first commit

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,33 @@
+package com.example.demo.student;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student mark = new Student(
+                    "Mark",
+                    "mark.lawrence@gmail.com",
+                    LocalDate.of(2000, Month.JANUARY, 5),
+                    24);
+
+            Student alex = new Student(
+                    "Alex",
+                    "alex.lawrence@gmail.com",
+                    LocalDate.of(2000, Month.APRIL, 9),
+                    24);
+            repository.saveAll(
+                    List.of(mark, alex)
+            );
+        };
+    }
+}
---
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -0,0 +1,25 @@
+package com.example.demo.student;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/v1/student") // Adjusted the mapping path for the controller
+public class StudentController {
+
+    private final StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping("/")
+    public List<Student> gStudentClasses() {
+        return studentService.gStudentClasses();
+    }
+}
